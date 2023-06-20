0c68515b3d88b6251e0563cd36d2b56adf702bbc
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-06-20 16:43:30 +0800

    update bio link

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,7 @@ const App = () => {
 
       <div className='flex flex-col text-neutral-300 ml-2 mt-10'>
         <a className='hover:text-neutral-500' href='https://github.com/RexanWONG/trending-cryptocurrencies-news-aggregator'>Github</a>
-        <a className='hover:text-neutral-500' href='https://github.com/RexanWONG/trending-cryptocurrencies-news-aggregator'>Created by rexanwong</a>
+        <a className='hover:text-neutral-500' href='https://linktr.ee/rexanwong'>Created by rexanwong</a>
 
       </div>
       
